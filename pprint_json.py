3a19a12715d84aab00397c8f2642d8b6b87a2fe1
Старовойтов Антон Станиславович <AStarovoytov@alfabank.ru>
2017-10-11 18:41:25 +0500

    Убрал ненужный requirements, подправил  не явные переменные

--- pprint_json.py
+++ pprint_json.py
@@ -6,8 +6,8 @@ def load_data(filepath, encoding_type):
         return json.load(f_handler)
 
 
-def pretty_print_json(data):
-    return json.dumps(data, indent=4, sort_keys=True)
+def pretty_print_json(parsed_json_data):
+    return json.dumps(parsed_json_data, indent=4, sort_keys=True)
 
 
 if __name__ == '__main__':
