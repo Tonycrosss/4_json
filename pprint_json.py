e80f08b2db663d5cdce2b87ec1d72ce6820fbee9
Старовойтов Антон Станиславович <AStarovoytov@alfabank.ru>
2017-10-13 13:44:33 +0500

    Исправил кодирование кириллицы

--- pprint_json.py
+++ pprint_json.py
@@ -3,15 +3,19 @@ import sys
 
 
 def load_data(filepath):
-    with open(filepath, 'r') as f_handler:
+    with open(filepath, 'r', encoding='utf-8') as f_handler:
         return json.load(f_handler)
 
 
-def pretty_print_json(parsed_json_data):
-    return json.dumps(parsed_json_data, indent=4, sort_keys=True)
+def pretty_json_formatter(parsed_json_data):
+    return json.dumps(parsed_json_data,
+                      indent=4,
+                      sort_keys=True,
+                      ensure_ascii=False)
 
 
 if __name__ == '__main__':
     filepath = sys.argv[1]
     parsed_json = load_data(filepath)
-    print(pretty_print_json(parsed_json))
+    print(pretty_json_formatter(parsed_json))
+
